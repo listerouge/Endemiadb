2c2d90000476cb9083112a0d7d56a31ecfa1d9e0
listerouge <listerouge@endemia.nc>
2015-11-04 17:16:14 +1100

    import-occur-pittos-actualisation

--- mysql/requete/chargementdata.sql
+++ mysql/requete/chargementdata.sql
@@ -1,2 +1,2 @@
-LOAD DATA LOCAL INFILE 'C:/Users/Endemia/Documents/12 - GestionDonnees/table-acropogon/tabOccurrence.csv' INTO TABLE occurence
+LOAD DATA LOCAL INFILE 'C:/Users/Endemia/Documents/12 - GestionDonnees/table-Pittos/tabOccurence.csv' INTO TABLE occurence
 FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n'

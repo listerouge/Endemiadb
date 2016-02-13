cbb6fd7ed284104134124387938908cbf1e1dd16
listerouge <listerouge@endemia.nc>
2016-02-13 14:49:04 +1100

    modificationSIS

--- mysql/requete/SIScountries.sql
+++ mysql/requete/SIScountries.sql
@@ -1,4 +1,4 @@
-SELECT idEvaluationListeRouge AS 'internal_taxon_id', PaysOccurence AS 'CountryOccurence.CountryOccurenceSubfield.CountryOccurenceLookup',Presence AS 'CountryOccurence.CountryOccurenceSubfield.presence',Origine AS 'CountryOccurence.CountryOccurenceSubfield.origin' FROM endemiadb.airegeographique
+SELECT idEvaluationListeRouge AS 'internal_taxon_id', PaysOccurence AS 'countryoccurrence.countryoccurrencesubfield.countryoccurrencelookup',Presence AS 'countryoccurrence.countryoccurrencesubfield.presence',Origine AS 'countryoccurrence.countryoccurrencesubfield.origin' FROM endemiadb.airegeographique
 INNER JOIN jointairegeographiqueevaluation
 ON idAireGeographique=AireGeographique_idAireGeographique
 INNER JOIN evaluationlisterouge
--- mysql/requete/SIStaxonomy.sql
+++ mysql/requete/SIStaxonomy.sql
@@ -1,4 +1,4 @@
-SELECT idEvaluationListeRouge AS 'internal_taxon_id', Regne AS 'kingdom', Phylum AS 'phylum', Classe AS 'classname', Ordre AS 'ordername', Famille AS 'family', Genre AS 'genus', Espece AS 'species', InfraType AS 'infraType', InfraNom AS 'infra_name', AutoriteTaxonomiqueEspece AS'taxonomicAuthority', AutoriteTaxonomiqueInfra AS 'infra_authority' FROM taxon
+SELECT idEvaluationListeRouge AS 'internal_taxon_id', Regne AS 'kingdom', Phylum AS 'phylum', ClasseBota AS 'classname', Ordre AS 'ordername', Famille AS 'family', Genre AS 'genus', Espece AS 'species', InfraType AS 'infratype', InfraNom AS 'infra_name', AutoriteTaxonomiqueEspece AS'taxonomicauthority', AutoriteTaxonomiqueInfra AS 'infra_authority ' FROM taxon
 INNER JOIN evaluationlisterouge
 On NomScientifique=Taxon_NomScientifique
 WHERE NomScientifique like '%Hibbertia%' 
